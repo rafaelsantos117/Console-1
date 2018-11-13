ee3f083e5d3c81bc22b5e1ba9e9cdc0508bd6e4a
rafaelsantos117 <rafael.santos117@fatec.sp.gov.br>
2018-11-13 00:46:23 -0200

    Teste Git 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Teste Git");
+            Console.WriteLine("Teste Gitt");
             Console.ReadKey();
         }
     }
